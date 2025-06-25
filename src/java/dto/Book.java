f0a5017b2dba3dc28874aa893ad85f52f14b59ae
Dương Minh Kiệt <toilakiet24@gmail.com>
2025-06-25 14:24:51 +0700

    Update

--- src/java/dto/Book.java
+++ src/java/dto/Book.java
@@ -6,8 +6,103 @@ package dto;
 
 /**
  *
- * @author SE190585
+ * @author toila
  */
 public class Book {
+    String title;
+    String author;
+    String isbn;
+    String category;
+    int published_year;
+    int total_copies;
+    int available_copies;
+    String status;
+    int id;
+    public Book(int id, String title, String author, String isbn, String category, int published_year, int total_copies, int available_copies, String status) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.category = category;
+        this.published_year = published_year;
+        this.total_copies = total_copies;
+        this.available_copies = available_copies;
+        this.status = status;
+    }
+ 
+    public Book() {
+    }
+    public int getId(){
+        return this.id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPublished_year() {
+        return published_year;
+    }
+
+    public void setPublished_year(int published_year) {
+        this.published_year = published_year;
+    }
+
+    public int getTotal_copies() {
+        return total_copies;
+    }
+
+    public void setTotal_copies(int total_copies) {
+        this.total_copies = total_copies;
+    }
+
+    public int getAvailable_copies() {
+        return available_copies;
+    }
+
+    public void setAvailable_copies(int available_copies) {
+        this.available_copies = available_copies;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{"  + ", title=" + title + ", author=" + author + ", isbn=" + isbn + ", category=" + category + ", published_year=" + published_year + ", total_copies=" + total_copies + ", available_copies=" + available_copies + ", status=" + status + '}';
+    }
     
 }
